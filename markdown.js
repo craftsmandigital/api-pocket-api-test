5b51719771c352c883863738b222ef80cda83c51
Jon <htcjon10@gmail.com>
2019-11-15 19:48:30 +0100

    Added tags into template markdownfile

---
+++ markdown.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+
+const json = require("./pocket-data.json");
+
+
+  const md = new Promise(function(resolve, reject) {
+    fs.readFile("./all-my-tags.md", "utf8", function(err, contents) {
+      resolve(contents);
+    });
+  });
+
+//   const json = new Promise(function(resolve, reject) {
+//     fs.readFile("./pocket-data.json", "utf8", function(err, contents) {
+//       resolve(contents);
+//     });
+//   });
+// let prReadmd = message => {
+//   return new Promise(function(resolve, reject) {
+//     fs.readFile("./all-my-tags.md", "utf8", function(err, contents) {
+//       resolve(contents);
+//     });
+//   });
+// };
+
+// let prReadjson = message => {
+//   return new Promise(function(resolve, reject) {
+//     fs.readFile("./pocket-data.json", "utf8", function(err, contents) {
+//       resolve(contents);
+//     });
+//   });
+// };
+// Promise.all([prReadjson(), prReadmd()]).then(function(values) {
+//   console.log(values);
+// });
+Promise.all([md]).then(function(contents) {
+  const outputList = json.map(x => ' "' + x.tag + '"').toString();
+  const token = "__TAGS__";
+  const res = contents[0].replace(token, outputList);
+  console.log(res);
+//   console.log(outputList);
+});
+
+// fs.readFile('./all-my-tags.md', 'utf8', function(err, contents) {
+//  const token = "__TAGS__";
+// const res = contents.replace(token, "W3Schools");
+// console.log(res);
+// });
